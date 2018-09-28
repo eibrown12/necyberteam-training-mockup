a364410925d135301180beb7ac2bb593bdf83d59
Eric Brown <eibrown12@gmail.com>
2018-09-28 12:59:12 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function($) {
+  "use strict"; // Start of use strict
+	
+  // Floating label headings for the contact form
+  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+  }).on("focus", ".floating-label-form-group", function() {
+    $(this).addClass("floating-label-form-group-with-focus");
+  }).on("blur", ".floating-label-form-group", function() {
+    $(this).removeClass("floating-label-form-group-with-focus");
+  });
+
+   
+  $(document).scroll(function () {
+	var doc = $(this).height();
+	var win = $(window).height();
+	if ( doc > win ) {
+		var $body = $("body");
+		var $nav = $("#mainNav");
+		var $secNav = $("#secondaryNav");
+		var $combined = $nav.height() + $secNav.height();
+		$nav.toggleClass('scrolled shadow-sm', $(this).scrollTop() > $combined);
+		$secNav.toggleClass('scrolled my-5', $(this).scrollTop() > $combined);
+	}
+  });
+})(jQuery); // End of use strict
